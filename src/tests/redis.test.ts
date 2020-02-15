82d44e590002e7c30b014e561d6ac47c5b6a0525
Etienne Martin <git@emartin.ca>
2020-02-15 18:05:24 -0500

    redis test

--- src/tests/redis.test.ts
+++ src/tests/redis.test.ts
@@ -4,13 +4,17 @@ import { app } from "./server";
 const request = supertest(app);
 
 describe("Redis", () => {
-  test("should cache responses in redis", async () => {
+  test("should cache responses in redis", async done => {
     const url = "/redis";
     await request.get(url);
-    const res = await request.get(url);
 
-    expect(res.status).toBe(200);
-    expect(res.header["x-cache"]).toEqual("HIT");
-    expect(res.text).toEqual("hello world");
+    setTimeout(async () => {
+      const res = await request.get(url);
+
+      expect(res.status).toBe(200);
+      expect(res.header["x-cache"]).toEqual("HIT");
+      expect(res.text).toEqual("hello world");
+      done();
+    }, 3000);
   });
 });
