8bd4920ae03c90486de2026868ad10ea7dd6f8a1
Etienne Martin <git@emartin.ca>
2020-03-18 13:45:40 -0400

    fix tests

--- src/tests/redis.test.ts
+++ src/tests/redis.test.ts
@@ -1,11 +1,8 @@
 import Redis from "ioredis";
 import { sharedTests } from "./test.shared";
 
-const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-
-beforeEach(async () => {
+afterEach(async () => {
   // Flush redis db before tests
-  await delay(100);
   const client = new Redis("//localhost:6379");
   await client.flushall();
 });
