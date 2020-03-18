33a5898a8d4166306e2dad2740388121d8571594
Etienne Martin <git@emartin.ca>
2020-03-18 11:50:35 -0400

    fix tests

--- src/tests/redis.test.ts
+++ src/tests/redis.test.ts
@@ -4,10 +4,7 @@ import { sharedTests } from "./test.shared";
 beforeEach(async () => {
   // Flush redis db before tests
   const client = new Redis("//localhost:6379");
-
   await client.flushall();
-
-  console.log("expiration", await client.keys("*"));
 });
 
 sharedTests("redis", 100);
