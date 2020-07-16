badb07e982a3fd0666a987ea61b2fde4bd46722f
Patrick Roy <patroytall@gmail.com>
2020-07-16 10:51:33 -0400

    Export PurgeFunction to library

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Options } from "./types";
 
 export { memoryStore } from "./stores/memory.store";
 export { redisStore } from "./stores/redis.store";
-export { GetCacheKey } from "./types";
+export { GetCacheKey, PurgeFunction } from "./types";
 
 export type ExpressAggressiveCacheOptions = Options;
 export default expressAggressiveCache;
