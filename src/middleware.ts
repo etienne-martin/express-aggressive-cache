42d5f30dc9f13dcd93868acf68f6496159d4c66b
Patrick Roy <patroytall@gmail.com>
2020-07-20 09:49:34 -0400

    fix build

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,7 +60,7 @@ export const expressAggressiveCache = (options?: Options) => {
       const originalWrite: any = res.write;
       const originalEnd: any = res.end;
       const chunkQueue = new Queue();
-      const normalizedUrl = defaultGetCacheKey({ req, res });
+      const normalizedUrl = defaultGetCacheKey({ req });
       const cacheKey = await getCacheKey({ req, res, normalizedUrl });
       const cachedResponse = await responseBucket.get(cacheKey);
 
