21e517d15290a94ba64b76af1ab5a7f3e3cc2c3d
Patrick Roy <patroytall@gmail.com>
2020-07-21 12:47:06 -0400

    Limit cache tag config to Redis

--- src/tests/compiled.router.ts
+++ src/tests/compiled.router.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import expressAggressiveCache from "../../dist"; // <-- Using the compiled version
 import { sharedRoutes } from "./shared.routes";
-import { akamaiGetCacheTag } from "./akamai-cache-tag";
 
 export const compiledRouter = Router();
-export const cache = expressAggressiveCache({ getCacheTag: akamaiGetCacheTag });
+export const cache = expressAggressiveCache();
 
 compiledRouter.use(cache.middleware);
 compiledRouter.use(sharedRoutes);
